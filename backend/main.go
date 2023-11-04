8ca35691d81a05feaddd138c35c621e223628046
RNAV2022 <ryannav2019@gmail.com>
2023-11-04 11:34:25 +0000

    Removed loading env file

--- backend/main.go
+++ backend/main.go
@@ -3,15 +3,15 @@ package main
 import (
 	"rnav2022/avenue/model"
 	"rnav2022/avenue/server"
-
-	"github.com/joho/godotenv"
+	// "github.com/joho/godotenv"
 )
 
 func main() {
-	err := godotenv.Load()
-	if err != nil {
-		panic(err)
-	}
+	// Development code only
+	// err := godotenv.Load()
+	// if err != nil {
+	// 	panic(err)
+	// }
 
 	model.Setup()
 	server.SetupAndListen()
